34233fc147a38eb6f6ca50ca4c62e8a1fc9b0d42
ipadorusa <ipadorusa@gmail.com>
2023-03-06 11:12:09 +0900

    shit

--- app/blog/list.tsx
+++ app/blog/list.tsx
@@ -1,40 +1,15 @@
 'use client'
 
-import type { NextPage } from 'next'
-import Image from 'next/image'
-import Link from 'next/link'
-interface BlogData {
-  createdAt: string
-  name: string
-  avatar: string
-  title: number
-  id: string
-}
+//@ts-ignore
+async function List({ recentPosts }) {
+  const albums = await recentPosts
 
-interface HomeProps {
-  recentPosts: BlogData[]
-}
-
-// This is a Client Component. It receives data as props and
-// has access to state and effects just like Page components
-// in the `pages` directory.
-const List: NextPage<HomeProps> = ({ recentPosts }) => {
   return (
-    <div className="blog__container">
-      <ul className="blog__list">
-        {recentPosts.map((post) => (
-          <li key={post.id} className="item">
-            <Link href={`/blog/${post.id}`}>
-              <div className="img">
-                <Image src={post.avatar} alt={post.name} width={128} height={128} />
-              </div>
-              <strong className="title">{post.title}</strong>
-              <span className="name">{post.name}</span>
-            </Link>
-          </li>
-        ))}
-      </ul>
-    </div>
+    <ul>
+      {albums.map((album: any) => (
+        <li key={album.id}>{album.name}</li>
+      ))}
+    </ul>
   )
 }
 
--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,11 +1,10 @@
 'use client'
-import List from './list'
+
+import { Suspense } from 'react'
 
 async function getPosts() {
-  const res = await fetch('https://hacker-news.firebaseio.com/v0/item/8863.json?print=pretty')
-
+  const res = await fetch('https://63d271c606556a0fdd3c55ed.mockapi.io/articles')
   const posts = await res.json()
-  console.log('posts ', posts)
   return posts
 }
 
@@ -13,5 +12,22 @@ export default async function Page() {
   // Fetch data directly in a Server Component
   const recentPosts = await getPosts()
   // Forward fetched data to your Client Component
-  return <List recentPosts={recentPosts} />
+  //@ts-ignore
+  return (
+    <Suspense>
+      <List recentPosts={recentPosts} />
+    </Suspense>
+  )
+}
+//@ts-ignore
+async function List({ recentPosts }: { recentPosts: any }): Promise<JSX.Element> {
+  const albums = await recentPosts
+
+  return (
+    <ul>
+      {albums.map((album: any) => (
+        <li key={album.id}>{album.name}</li>
+      ))}
+    </ul>
+  )
 }
